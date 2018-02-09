a621cb353f3d05df31b60f04fed4ab09031de0e0
mshalabi <mohamad.shalabi@mytoys.de>
2018-02-09 16:44:30 +0100

    initial project creating

---
+++ src/main/java/com/learn/domaidrivedesign/DomaiDriveDesignApplication.java
@@ -0,0 +1,12 @@
+package com.learn.domaidrivedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaiDriveDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaiDriveDesignApplication.class, args);
+	}
+}
